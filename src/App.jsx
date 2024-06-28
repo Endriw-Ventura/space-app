ee9b16b7a8912368973970eaa15e101fda228a58
Endriw Ventura <endriwventura@yahoo.com>
2024-06-27 21:46:44 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import GlobalStyles from "./components/GlobalStyles";
+
+const GradientBackground = styled.div`
+background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+width: 100%;
+min-height: 100vh;
+`
+
+function App() {
+  return (
+    <GradientBackground>
+     <GlobalStyles />
+    </GradientBackground>
+  )
+}
+
+export default App
