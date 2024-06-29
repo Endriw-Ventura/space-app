4d00ca079795de68cff8786e184744b1324ed4ed
Endriw Ventura <endriwventura@yahoo.com>
2024-06-29 19:32:58 -0300

    #UPDATE - Added the navigator component and the banner component to the main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import GlobalStyles from "./components/GlobalStyles";
 import Header from "./components/HeaderStyled";
+import NavigationList from "./components/NavigationList";
+import Banner from "./components/Banner";
+
 
 const GradientBackground = styled.div`
 background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -10,11 +13,21 @@ padding: 0 24px;
 box-sizing: border-box;
 `
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 24px;
+`;
+
 function App() {
   return (
     <GradientBackground>
      <GlobalStyles />
      <Header />
+     <Container>
+        <NavigationList />
+        <Banner />
+     </Container>
     </GradientBackground>
   )
 }
